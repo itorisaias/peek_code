caec0308a3258a460629952a9a6310a8b9aeaba8
Gustavo Oliveira <g.92oliveira@gmail.com>
2021-02-27 10:56:46 -0300

    verify if payment exist

--- lib/peek_code/payments.ex
+++ lib/peek_code/payments.ex
@@ -1,4 +1,6 @@
 defmodule PeekCode.Payments do
+  import Ecto.Query, only: [from: 2]
+
   alias Ecto.Multi
   alias PeekCode.Orders.Order
   alias PeekCode.Payment.Payment
@@ -13,12 +15,34 @@ defmodule PeekCode.Payments do
   defp build_response(nil, _), do: {:error, "this order does not exist"}
 
   defp build_response(order, payload) do
-    {:ok, %{payment: payment}} =
-      Multi.new()
-      |> Multi.update(:order, Order.update_balance_due(order, %{amount: payload.amount}))
-      |> Multi.insert(:payment, Payment.changeset(%Payment{}, payload))
-      |> Repo.transaction()
+    verify_if_payment_exist(order, payload)
+    |> case do
+      true ->
+        {:error, "Payment duplicated"}
 
-    {:ok, payment}
+      false ->
+        {:ok, %{payment: payment}} =
+          Multi.new()
+          |> Multi.update(:order, Order.update_balance_due(order, %{amount: payload.amount}))
+          |> Multi.insert(:payment, Payment.changeset(%Payment{}, payload))
+          |> Repo.transaction()
+
+        {:ok, payment}
+    end
+  end
+
+  defp verify_if_payment_exist(order, payload) do
+    payments =
+      from(p in Payment,
+        where:
+          p.order_id == ^order.id and
+            p.amount ==
+              ^payload.amount and
+            p.applied_at ==
+              ^payload.applied_at
+      )
+      |> Repo.aggregate(:count, :id)
+
+    payments > 0
   end
 end
