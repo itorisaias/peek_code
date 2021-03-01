1ce76a95429c93075e37543ffa777df66d4b8e1e
Gustavo Oliveira <g.92oliveira@gmail.com>
2021-03-01 07:33:47 -0300

    Automatically deploy to Gigalixir

--- config/releases.exs
+++ config/releases.exs
@@ -24,6 +24,7 @@ config :peek_code, PeekCodeWeb.Endpoint,
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
   secret_key_base: secret_key_base
 
 config :peek_code, PeekCodeWeb.Endpoint, server: true
